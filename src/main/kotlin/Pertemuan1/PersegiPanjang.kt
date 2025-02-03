073b107363a13e0594ae66ca96560a3f864c6e8a
Lisvindanu <Lisvindanu015@gmail.com>
2025-02-03 17:41:27 +0700

    Memperbaiki kode pada class PersegiPanjang.kt dan menambahkan Readme.md

--- src/main/kotlin/Pertemuan1/PersegiPanjang.kt
+++ src/main/kotlin/Pertemuan1/PersegiPanjang.kt
@@ -2,7 +2,7 @@ package Praktikum.Pemrograman.Mobile.Lisvindanu.Pertemuan1
 
 import java.util.*
 
-class PersegiPanjang(val panjang: Int? = null, lebar: Int ? = null) {
+class PersegiPanjang(val panjang: Int? = null, val lebar: Int ? = null) {
         fun hitungLuas (panjang : Int, lebar: Int): Int {
              return panjang * lebar
         }
@@ -33,12 +33,14 @@ class PersegiPanjang(val panjang: Int? = null, lebar: Int ? = null) {
         //Menggunakan Parameter Argument pada constructor
 
         val persegiPanjang2 = PersegiPanjang(4, 2)
+        println("Nilai Panjang ${persegiPanjang2.panjang} ")
+        println("Nilai Lebar ${persegiPanjang2.lebar} ")
         println("Hasil Luas Persegi Panjang "+ persegiPanjang2.hitungLuas(panjang, lebar))
         println("Hasil Keliling Persegi Panjang " + persegiPanjang2.hitungKeliling(panjang, lebar))
 
         println("=================================")
 
-        //Memasukkan Nilai langsung pada fungsi
+        // Menggunakan Nilai langsung di fungsi (tanpa properti kelas)
         val persegiPanjang3 = PersegiPanjang()
         println("Hasil Luas Persegi Panjang "+ persegiPanjang3.hitungLuas(4, 2))
         println("Hasil Keliling Persegi Panjang " + persegiPanjang3.hitungKeliling(4, 2))
